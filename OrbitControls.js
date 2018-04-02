cc0d2f6d133eff08d7dc384492a04b3d11d19e7d
antonfed <fed.anton@gmail.com>
2018-04-02 14:33:43 +0300

    Update OrbitControls.js

--- OrbitControls.js
+++ OrbitControls.js
@@ -808,7 +808,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		switch ( event.touches.length ) {
 
-			case 1:	// one-fingered touch: rotate
+			case 3:	// one-fingered touch: rotate
 
 				if ( scope.enableRotate === false ) return;
 
@@ -828,7 +828,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 				break;
 
-			case 3: // three-fingered touch: pan
+			case 1: // three-fingered touch: pan
 
 				if ( scope.enablePan === false ) return;
 
@@ -861,7 +861,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		switch ( event.touches.length ) {
 
-			case 1: // one-fingered touch: rotate
+			case 3: // one-fingered touch: rotate
 
 				if ( scope.enableRotate === false ) return;
 				if ( state !== STATE.TOUCH_ROTATE ) return; // is this needed?...
@@ -879,7 +879,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 				break;
 
-			case 3: // three-fingered touch: pan
+			case 1: // three-fingered touch: pan
 
 				if ( scope.enablePan === false ) return;
 				if ( state !== STATE.TOUCH_PAN ) return; // is this needed?...
